69f5e3262101b46f282d5ebb94b91004e44bfc72
hossainmamun <hossainmamun337@gmailcom>
2021-02-07 20:08:30 +0600

    hotel cost commit

--- assignment.js
+++ assignment.js
@@ -1,6 +1,3 @@
-// repository link
-// https://github.com/hossainmamun/assignment-js
-
 
 // convert kilometer to meter
 function kilometerToMeter(kilometer){
@@ -21,5 +18,32 @@ function budgetCalculator(watch, phone, laptop){
 }
 let budget = budgetCalculator(50, 100, 500);
 console.log(budget);
+
+
+// hotel cost
+var rent = 0;
+function hotelCost(days){
+    if(days <= 10){
+        rent = days * 100;
+        return rent;
+    }
+    else if(days <= 20){
+        var firstPackage = 10 * 100;
+        var remaining = days - 10;
+        var secondPackage = remaining * 80;
+        rent = firstPackage + secondPackage;
+        return rent;
+    }
+    else{
+        var firstPackage = 10 * 100;
+        var secondPackage = 10 * 80;
+        var remaining = days - 20;
+        var thirdPackage = remaining * 50;
+        rent = firstPackage + secondPackage + thirdPackage;
+        return rent;
+    }
+}
+let totalRent = hotelCost(51);
+console.log(totalRent);
 
 
